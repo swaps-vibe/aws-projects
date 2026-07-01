a19d3c78db3bc40dc750c3144c9aefa9d772d5aa
swaps-vibe <swapnatamshettepatil@gmail.com>
2026-07-01 21:44:51 +0530

    Added python day1 programs

---
+++ python-programs/day1.py
@@ -0,0 +1,12 @@
+def sum_numbers(numbers):
+     total = 0
+     for num in numbers:
+        total=total+num
+    
+        print( f"sum is :{total}" )
+        return total
+
+numbers = [10,20,30,40] 
+    
+sum_numbers(numbers)   
+    
